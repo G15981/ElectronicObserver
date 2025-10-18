82ce0c38986da7029a2814823042d346d2413658
1 <1>
2025-10-18 22:36:53 +0800

    舰队、舰队一览、入渠分割线设置为bg2

---
+++ ElectronicObserver/Window/Wpf/ConfigBrushes.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media;
+using ElectronicObserver.Utility;
+
+namespace ElectronicObserver.Window.Wpf
+{
+	public static class ConfigBrushes
+	{
+		public static SolidColorBrush SubBackBrush => new(Configuration.Config.UI.SubBackColor.ToWpfColor());
+	}
+}
