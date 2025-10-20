270eaae60ab1859cc0605a3189e3ae7dff260421
1 <1>
2025-10-20 14:27:58 +0800

    修复战斗窗口击沉色块bug

--- ElectronicObserver/Window/Wpf/Battle/BattleViewModel.cs
+++ ElectronicObserver/Window/Wpf/Battle/BattleViewModel.cs
@@ -1008,8 +1008,8 @@ public partial class BattleViewModel : AnchorableViewModel
 			HPBars[index].BackColor = HPBars[index] switch
 			{
 				//{ Value: < 1, IsTargetable: true } => Color.FromArgb(0x40, 0x4D, 0xA6, 0xDF),
-				//_ => Color.Transparent,
 				{ Value: < 1, IsTargetable: true } => Color.Transparent,
+				_ => Color.Transparent,
 			};
 		}
 
