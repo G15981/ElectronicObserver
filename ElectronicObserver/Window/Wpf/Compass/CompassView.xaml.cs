8aa112ff58b043c7919bd4fac25fa371565d1140
1 <1>
2025-12-23 19:16:42 +0800

    添加罗盘窗口全局滚动条 原版只有进点前阵容N选1的时候提供有限滚动条 修改后整个罗盘页面都支持滚动条，有垂直滚动条时滚轮控制垂直，没有垂直时滚轮控制水平，都有时按住shift控制水平。 同时关闭EnemyListView.xaml中原有的滚动条

--- ElectronicObserver/Window/Wpf/Compass/CompassView.xaml.cs
+++ ElectronicObserver/Window/Wpf/Compass/CompassView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ElectronicObserver.Window.Wpf.Compass;
 
@@ -21,4 +22,33 @@ public partial class CompassView : UserControl
 	{
 		InitializeComponent();
 	}
+	private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+	{
+		var sv = (ScrollViewer)sender;
+
+		// 按住shift键 → 水平滚
+		if ((Keyboard.Modifiers & ModifierKeys.Shift) != 0)
+		{
+			sv.ScrollToHorizontalOffset(sv.HorizontalOffset - e.Delta);
+			e.Handled = true;
+			return;
+		}
+
+		// 有垂直滚动能力 → 用垂直滚
+		if (sv.ScrollableHeight > 0)
+		{
+			// 默认行为即可
+			return;
+		}
+
+		// 没有垂直滚动 → 改成水平滚
+		if (sv.ScrollableWidth > 0)
+		{
+			sv.ScrollToHorizontalOffset(sv.HorizontalOffset - e.Delta);
+			e.Handled = true;
+		}
+
+
+	}
+
 }
